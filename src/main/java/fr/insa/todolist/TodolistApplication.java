245edcfd3cb2d214ef9aaf8a0096423e144e1938
MrADOY <pietrzak.aurelien@gmail.com>
2021-11-20 10:06:47 +0100

    add comment

--- src/main/java/fr/insa/todolist/TodolistApplication.java
+++ src/main/java/fr/insa/todolist/TodolistApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodolistApplication {
 
+	/* methode main */
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
 	}
